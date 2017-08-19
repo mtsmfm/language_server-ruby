f7e38636b6f8e9149e7a5f4250e77d00ed27a202
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2017-08-20 01:31:01 +0900

    Test against old ruby

--- test/language_server/linter/ruby_wc_test.rb
+++ test/language_server/linter/ruby_wc_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 module LanguageServer::Linter
   class RubyWCTest < Minitest::Test
     def test_error
-      linter = RubyWC.new(<<~EOS)
+      linter = RubyWC.new(<<-EOS.strip_heredoc)
         require "foo
         if a == "\\n"
       EOS
@@ -14,9 +14,9 @@ module LanguageServer::Linter
     end
 
     def test_warn
-      linter = RubyWC.new(<<~RUBY)
+      linter = RubyWC.new(<<-EOS.strip_heredoc)
         a = 1
-      RUBY
+      EOS
 
       assert {
         linter.call == [Error.new(line_num: 0, message: "assigned but unused variable - a", type: "warning")]

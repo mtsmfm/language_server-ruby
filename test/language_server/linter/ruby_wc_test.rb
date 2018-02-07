c484cdbe8dc8d60d1e1174d0abc017ef7f0b9eaa
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2018-02-07 12:39:01 +0900

    Fix test

--- test/language_server/linter/ruby_wc_test.rb
+++ test/language_server/linter/ruby_wc_test.rb
@@ -8,8 +8,19 @@ module LanguageServer::Linter
         if a == "\\n"
       EOS
 
+      # <compiled>:2: syntax error, unexpected $undefined, expecting end-of-input
+      # if a == "\n"
+      #           ^
+
+      characters =
+        if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.5.0")
+          9..10
+        else
+          10..11
+        end
+
       assert {
-        linter.call == [Error.new(line_num: 1, characters: 10..11, message: "unexpected $undefined, expecting end-of-input", type: "syntax error")]
+        linter.call == [Error.new(line_num: 1, characters: characters, message: "unexpected $undefined, expecting end-of-input", type: "syntax error")]
       }
     end
 
